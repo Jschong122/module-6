7af54ad1a4eab665481a9b12b5e923af0665aa50
Jschong122 <cilmbv03918@gmail.com>
2024-07-04 17:17:49 +0800

    updated lab4 and lab2

--- react-sandbox/src/containers/LabFour.jsx
+++ react-sandbox/src/containers/LabFour.jsx
@@ -1,26 +1,11 @@
 import React from "react";
 import { BigCats } from "../components/BigCats";
 
-export const LabFour = () => {
-  //function for reverse the list    ,  function for alphabetically sorting the list
-
-  // display only cat family - ‘Panthera’
-
-  //button
-
-  const BtnStyle = {
-    padding: "5px",
-    margin: "20px",
-    display: "flex",
-    justifyContent: "space-evenly",
-  };
-
+export function LabFour() {
   return (
     <div>
-      <p> Lab 4</p>
-      {/* <button style={BtnStyle}> Display only Panthera </button>
-      <button style={BtnStyle}> Reverse List </button> */}
-      <BigCats></BigCats>
+      <p>Lab 4</p>
+      <BigCats showFormAndDelete={false} showResetButton={true} />
     </div>
   );
-};
+}
